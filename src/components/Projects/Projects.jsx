35070802b31e4e126f17c409b88868fccaaef52a
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-07-19 11:35:26 -0500

    feat: added Project component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import projImg1 from "../../assets/img/project-img1.png";
+import colorSharp2 from "../../assets/img/color-sharp2.png";
+import TrackVisibility from "react-on-screen";
+import { ProjectCard } from "./ProjectCard";
+import "./Projects.css";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Proyectos</h2>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Reiciendis accusamus deleniti cum quibusdam laboriosam
+                    accusantium blanditiis esse voluptas reprehenderit iure
+                  </p>
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  );
 };
 
 export default Projects;
