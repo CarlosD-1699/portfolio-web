2736243e260865ee08b6cf17096916517cf60bb0
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-07-19 15:01:18 -0500

    feat: added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import NavBar from "./components/NavBar/NavBar";
 import Banner from "./components/Banner/Banner";
 import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer/Footer";
+import { Footer } from "./components/Footer/Footer";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
