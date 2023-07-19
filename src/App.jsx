16acaed78c9c81f1e3fb3de5c16ab615d802cdad
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-07-19 11:05:24 -0500

    feat: added Banner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Banner from "./components/Banner/Banner";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
